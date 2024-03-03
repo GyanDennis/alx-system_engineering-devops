3d5f90afa2ffb5413be8e8c4eed2ae8dc4e2a422
GyanDennis <126685558+GyanDennis@users.noreply.github.com>
2024-03-03 17:34:33 +0000

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# Script to fix:
-# 500 error when a GET HTTP method is requested to Apache web server
-
-exec {'replace':
-  provider => shell,
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
