5bc5751508f8b889768dfbd6924f9870c9598e38
GyanDennis <126685558+GyanDennis@users.noreply.github.com>
2024-03-23 14:06:42 +0000

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python3
-
 """
-prints the titles of the first 10 hot posts listed for a given subreddit
+Function to print hot posts on a given Reddit subreddit
 """
-
 import requests
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
+    Method to print the titles of the 10 hottest posts on a given subreddit
     """
 
-    if subreddit is None or not isinstance(subreddit, str):
-        print("Invalid subreddit name.")
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 404:
+        print("None")
         return
 
-    user_agent = {
-        'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
-    }
-    params = {'limit': 10}
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
-
-    try:
-        response = requests.get(url, headers=user_agent, params=params)
-        response.raise_for_status()  # Raise an exception for any HTTP errors
-
-        data = response.json()
-        posts = data['data']['children']
-
-        for post in posts:
-            title = post['data']['title']
-            print(title)
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-    except (KeyError, ValueError) as e:
-        print(f"Failed to parse the response: {e}")
-
-
-# Example usage
-top_ten('programming')
+    results = response.json().get("data")
+    # [print(c.get("data").get("title")) for c in results.get("children")]
+    for c in results.get("children", []):
+        print(c.get("data", {}).get("title"))
