ea662ab15f330edb2866e25b206f46e8864a0957
GyanDennis <126685558+GyanDennis@users.noreply.github.com>
2024-03-24 12:15:43 +0000

    Update 1-main.py

--- 0x16-api_advanced/1-main.py
+++ 0x16-api_advanced/1-main.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python3
-
-import requests
+#!/usr/bin/python3
+"""
+1-main
+"""
 import sys
 
-def fetch_top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot/.json?limit=10"
-    headers = {'User-Agent': 'MyBot/1.0'}  # Provide a proper User-Agent header
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        data = response.json()
-        for post in data['data']['children']:
-            print(post['data']['title'])
-    except requests.exceptions.HTTPError as err:
-        print(f"An error occurred: {err}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        fetch_top_ten(sys.argv[1])
+        top_ten(sys.argv[1])
